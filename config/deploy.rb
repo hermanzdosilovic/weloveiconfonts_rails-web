405cbe04d3435cbf1236d4d7f04226fccd8eb4c8
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-08-19 19:28:41 +0200

    Added mina deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+
+set :domain, 'shpaolin.ddns.net'
+set :deploy_to, '/var/www/weloveiconfonts_rails-web'
+set :repository, 'git@github.com:hermanzdosilovic/weloveiconfonts_rails-web.git'
+set :branch, 'master'
+set :user, 'titus'
+set :forward_agent, true
+
+task setup: :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+end
+
+desc "Deploys the current version to the server."
+task deploy: :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'bundle:install'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+    end
+  end
+end
+
+task :environment do
+  invoke :'rbenv:load'
+end
